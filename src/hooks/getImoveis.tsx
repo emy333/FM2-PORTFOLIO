8a3663b688cb4a4d5ccf71a6f6e6c1939086f3b0
Emelly Freitas <emedoim333@gmail.com>
2025-05-21 14:52:03 -0300

    ajuste

--- src/hooks/getImoveis.tsx
+++ src/hooks/getImoveis.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axiosInstance from "@/services/api";
 
 const getImoveis= async () => {
-  const { data } = await axiosInstance.get(`imoveis/imoveisPortfolio`);
+  const { data } = await axiosInstance.get(`imoveis/imoveisPortfolio/fm2-imobiliaria`);
   if (data.erro) {
     throw new Error("Imóveis não encontrados");
   }
